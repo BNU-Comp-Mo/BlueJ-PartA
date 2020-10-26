e36d7ff278bb94c49c9e898aec32d81493cd078d
MRwan-mo <72706653+MRwan-mo@users.noreply.github.com>
2020-10-26 13:42:26 +0000

    Create Course.java
    Created a new class called course

---
+++ app01-lab-classes/Course.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Course here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Course
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Course
+     */
+    public Course()
+    {
+        // initialise instance variables
+      
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+      
+    }
+}
